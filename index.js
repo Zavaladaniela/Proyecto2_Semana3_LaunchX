853ce4204932e1396a3ee9b649cfeec6e6029cc5
Zavaladaniela <daniela.zavala107@gmail.com>
2022-04-20 00:13:10 -0500

    Add the atributs for an spiderman object and run de test of that, the last passed

--- index.js
+++
@@ -1,3 +0,0 @@
-const spiderman = require ('./../app/heroInfo')
-
-const andrewGarfield = new spiderman ("The Amazing Spiderman",31,"Andrew Garfield",2,"Sony")
--- test/spidermantest.test.js
+++ test/spidermantest.test.js
@@ -4,16 +4,17 @@
         expect (resultOfSomething).toBe(3);
     });
 }) */
+const spiderman = require ('./../app/heroInfo')
 
 describe ("Unit Tests for Spiderman class", () => {
     test ('1) Create an spiderman object', () =>{
         const andrewGarfield = new spiderman ("The Amazing Spiderman",31,"Andrew Garfield",2,"Sony")
 
         //Validando datos
-        expect (andrewGarfield.name).toBe ("The Amagazing Spiderman")
+        expect (andrewGarfield.name).toBe("The Amazing Spiderman")
         expect (andrewGarfield.age).toBe(31)
         expect (andrewGarfield.actor).toBe("Andrew Garfield")
-        expect (andrewGarfield.movies).toBe(31)
+        expect (andrewGarfield.movies).toBe(2)
         expect (andrewGarfield.studio).toBe("Sony")
     });
 })
