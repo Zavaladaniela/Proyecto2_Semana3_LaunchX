a39466b11ee639af4c0a39dc733080c115aeb979
Zavaladaniela <daniela.zavala107@gmail.com>
2022-04-20 00:17:02 -0500

    Test with an atribut false and a test failed

--- test/spidermantest.test.js
+++ test/spidermantest.test.js
@@ -14,7 +14,7 @@ describe ("Unit Tests for Spiderman class", () => {
         expect (andrewGarfield.name).toBe("The Amazing Spiderman")
         expect (andrewGarfield.age).toBe(31)
         expect (andrewGarfield.actor).toBe("Andrew Garfield")
-        expect (andrewGarfield.movies).toBe(2)
+        expect (andrewGarfield.movies).toBe(31)
         expect (andrewGarfield.studio).toBe("Sony")
     });
 })
